7e0296004c29074967d0390bcece6a391326b4db
Michael Aitken <mike@mikeaitken.net>
2023-05-25 19:52:43 +0800

    Finalised Authentication

--- models/user.js
+++ models/user.js
@@ -1,4 +1,4 @@
-import { Schema, model, models } from "mongoose";
+import { Schema, model, models } from 'mongoose';
 
 const UserSchema = new Schema({
     email: {
@@ -9,7 +9,7 @@ const UserSchema = new Schema({
     username: {
         type: String,
         required: [true, 'Username is required!'],
-        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"],
+        match: [/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/, "Username invalid, it should contain 8-20 alphanumeric letters and be unique!"]
     },
     image: {
         type: String,
